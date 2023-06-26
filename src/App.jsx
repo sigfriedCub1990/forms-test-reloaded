0d97a2437e9fb71bd1f58faa73e40c1f4a841ee8
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-26 13:09:47 +0200

    feat: useReducer version

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,74 @@
 import * as React from 'react'
 import './App.css'
 
+const ACTIONS = {
+  START_DATE_UPDATE: 'start_date_update',
+  END_DATE_UPDATE: 'end_date_update',
+  MORNING_SHIFT_HIGH_UPDATE: 'morning_shift_high_update',
+  AFTERNOON_SHIFT_HIGH_UPDATE: 'afternoon_shift_high_update',
+  MORNING_SHIFT_LOW_UPDATE: 'morning_shift_low_update',
+  AFTERNOON_SHIFT_LOW_UPDATE: 'afternoon_shift_low_update',
+}
+
+const formReducer = (formState, action) => {
+  const { type } = action
+
+  switch (type) {
+    case ACTIONS.START_DATE_UPDATE: {
+      const field = { ...formState.startDate }
+      field.value = action.payload
+      return {
+        ...formState,
+        startDate: field,
+      }
+    }
+    case ACTIONS.END_DATE_UPDATE: {
+      const field = { ...formState.endDate }
+      field.value = action.payload
+      return {
+        ...formState,
+        endDate: field,
+      }
+    }
+    case ACTIONS.MORNING_SHIFT_HIGH_UPDATE: {
+      const field = { ...formState.morningShiftHigh }
+      field.value = action.payload
+      return {
+        ...formState,
+        morningShiftHigh: field,
+      }
+    }
+    case ACTIONS.AFTERNOON_SHIFT_HIGH_UPDATE: {
+      const field = { ...formState.afternoonShiftHigh }
+      field.value = action.payload
+      return {
+        ...formState,
+        afternoonShiftHigh: field,
+      }
+    }
+    case ACTIONS.MORNING_SHIFT_LOW_UPDATE: {
+      const field = { ...formState.morningShiftLow }
+      field.value = action.payload
+      return {
+        ...formState,
+        morningShiftLow: field,
+      }
+    }
+    case ACTIONS.AFTERNOON_SHIFT_LOW_UPDATE: {
+      const field = { ...formState.afternoonShiftLow }
+      field.value = action.payload
+      return {
+        ...formState,
+        afternoonShiftLow: field,
+      }
+    }
+    default:
+      return formState
+  }
+}
+
 function App() {
-  const [form, setForm] = React.useState({
+  const [form, dispatch] = React.useReducer(formReducer, {
     startDate: {
       value: '',
       required: true,
@@ -34,17 +100,6 @@ function App() {
     console.debug('Submit form')
   }
 
-  const onFieldChange = ({ target }) => {
-    const modifiedField = {
-      required: form[target.name].required,
-      value: target.value,
-    }
-    setForm({
-      ...form,
-      [target.name]: modifiedField,
-    })
-  }
-
   const areNsdConfigurationModalRequiedFieldFilled = (() => {
     const requiredFields = Object.keys(form).filter((key) => form[key].required)
     const areAllRequiredFieldsFilled = requiredFields.every(
@@ -65,7 +120,9 @@ function App() {
             <input
               name="startDate"
               value={form.startDate.value}
-              onChange={onFieldChange}
+              onChange={({ target: { value } }) =>
+                dispatch({ type: ACTIONS.START_DATE_UPDATE, payload: value })
+              }
               required={form.startDate.required}
             />
           </div>
@@ -76,7 +133,9 @@ function App() {
             <input
               name="endDate"
               value={form.endDate.value}
-              onChange={onFieldChange}
+              onChange={({ target: { value } }) =>
+                dispatch({ type: ACTIONS.END_DATE_UPDATE, payload: value })
+              }
               required={form.endDate.required}
             />
           </div>
@@ -89,7 +148,12 @@ function App() {
             <input
               name="morningShiftHigh"
               value={form.morningShiftHigh.value}
-              onChange={onFieldChange}
+              onChange={({ target: { value } }) =>
+                dispatch({
+                  type: ACTIONS.MORNING_SHIFT_HIGH_UPDATE,
+                  payload: value,
+                })
+              }
               required={form.morningShiftHigh.required}
             />
           </div>
@@ -100,7 +164,12 @@ function App() {
             <input
               name="afternoonShiftHigh"
               value={form.afternoonShiftHigh.value}
-              onChange={onFieldChange}
+              onChange={({ target: { value } }) =>
+                dispatch({
+                  type: ACTIONS.AFTERNOON_SHIFT_HIGH_UPDATE,
+                  payload: value,
+                })
+              }
               required={form.afternoonShiftHigh.required}
             />
           </div>
@@ -113,7 +182,12 @@ function App() {
             <input
               name="morningShiftLow"
               value={form.morningShiftLow.value}
-              onChange={onFieldChange}
+              onChange={({ target: { value } }) =>
+                dispatch({
+                  type: ACTIONS.MORNING_SHIFT_LOW_UPDATE,
+                  payload: value,
+                })
+              }
               required={form.morningShiftLow.required}
             />
           </div>
@@ -124,7 +198,12 @@ function App() {
             <input
               name="afternoonShiftLow"
               value={form.afternoonShiftLow.value}
-              onChange={onFieldChange}
+              onChange={({ target: { value } }) =>
+                dispatch({
+                  type: ACTIONS.AFTERNOON_SHIFT_LOW_UPDATE,
+                  payload: value,
+                })
+              }
               required={form.afternoonShiftLow.required}
             />
           </div>
