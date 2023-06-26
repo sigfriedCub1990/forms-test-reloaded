e40fd8ad08f3059bc3cc542527ac37fb8a3a63a7
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-26 13:28:06 +0200

    refactor: Subcomponent with useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,72 +2,6 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import './App.css'
 
-const ACTIONS = {
-  START_DATE_UPDATE: 'start_date_update',
-  END_DATE_UPDATE: 'end_date_update',
-  MORNING_SHIFT_HIGH_UPDATE: 'morning_shift_high_update',
-  AFTERNOON_SHIFT_HIGH_UPDATE: 'afternoon_shift_high_update',
-  MORNING_SHIFT_LOW_UPDATE: 'morning_shift_low_update',
-  AFTERNOON_SHIFT_LOW_UPDATE: 'afternoon_shift_low_update',
-}
-
-const formReducer = (formState, action) => {
-  const { type } = action
-
-  switch (type) {
-    case ACTIONS.START_DATE_UPDATE: {
-      const field = { ...formState.startDate }
-      field.value = action.payload
-      return {
-        ...formState,
-        startDate: field,
-      }
-    }
-    case ACTIONS.END_DATE_UPDATE: {
-      const field = { ...formState.endDate }
-      field.value = action.payload
-      return {
-        ...formState,
-        endDate: field,
-      }
-    }
-    case ACTIONS.MORNING_SHIFT_HIGH_UPDATE: {
-      const field = { ...formState.morningShiftHigh }
-      field.value = action.payload
-      return {
-        ...formState,
-        morningShiftHigh: field,
-      }
-    }
-    case ACTIONS.AFTERNOON_SHIFT_HIGH_UPDATE: {
-      const field = { ...formState.afternoonShiftHigh }
-      field.value = action.payload
-      return {
-        ...formState,
-        afternoonShiftHigh: field,
-      }
-    }
-    case ACTIONS.MORNING_SHIFT_LOW_UPDATE: {
-      const field = { ...formState.morningShiftLow }
-      field.value = action.payload
-      return {
-        ...formState,
-        morningShiftLow: field,
-      }
-    }
-    case ACTIONS.AFTERNOON_SHIFT_LOW_UPDATE: {
-      const field = { ...formState.afternoonShiftLow }
-      field.value = action.payload
-      return {
-        ...formState,
-        afternoonShiftLow: field,
-      }
-    }
-    default:
-      return formState
-  }
-}
-
 const Form = ({ onSubmit, form, dispatch, isSubmitDisabled }) => {
   return (
     <form onSubmit={onSubmit}>
@@ -79,9 +13,7 @@ const Form = ({ onSubmit, form, dispatch, isSubmitDisabled }) => {
           <input
             name="startDate"
             value={form.startDate.value}
-            onChange={({ target: { value } }) =>
-              dispatch({ type: ACTIONS.START_DATE_UPDATE, payload: value })
-            }
+            onChange={dispatch}
             required={form.startDate.required}
           />
         </div>
@@ -92,9 +24,7 @@ const Form = ({ onSubmit, form, dispatch, isSubmitDisabled }) => {
           <input
             name="endDate"
             value={form.endDate.value}
-            onChange={({ target: { value } }) =>
-              dispatch({ type: ACTIONS.END_DATE_UPDATE, payload: value })
-            }
+            onChange={dispatch}
             required={form.endDate.required}
           />
         </div>
@@ -107,12 +37,7 @@ const Form = ({ onSubmit, form, dispatch, isSubmitDisabled }) => {
           <input
             name="morningShiftHigh"
             value={form.morningShiftHigh.value}
-            onChange={({ target: { value } }) =>
-              dispatch({
-                type: ACTIONS.MORNING_SHIFT_HIGH_UPDATE,
-                payload: value,
-              })
-            }
+            onChange={dispatch}
             required={form.morningShiftHigh.required}
           />
         </div>
@@ -123,12 +48,7 @@ const Form = ({ onSubmit, form, dispatch, isSubmitDisabled }) => {
           <input
             name="afternoonShiftHigh"
             value={form.afternoonShiftHigh.value}
-            onChange={({ target: { value } }) =>
-              dispatch({
-                type: ACTIONS.AFTERNOON_SHIFT_HIGH_UPDATE,
-                payload: value,
-              })
-            }
+            onChange={dispatch}
             required={form.afternoonShiftHigh.required}
           />
         </div>
@@ -141,12 +61,7 @@ const Form = ({ onSubmit, form, dispatch, isSubmitDisabled }) => {
           <input
             name="morningShiftLow"
             value={form.morningShiftLow.value}
-            onChange={({ target: { value } }) =>
-              dispatch({
-                type: ACTIONS.MORNING_SHIFT_LOW_UPDATE,
-                payload: value,
-              })
-            }
+            onChange={dispatch}
             required={form.morningShiftLow.required}
           />
         </div>
@@ -157,12 +72,7 @@ const Form = ({ onSubmit, form, dispatch, isSubmitDisabled }) => {
           <input
             name="afternoonShiftLow"
             value={form.afternoonShiftLow.value}
-            onChange={({ target: { value } }) =>
-              dispatch({
-                type: ACTIONS.AFTERNOON_SHIFT_LOW_UPDATE,
-                payload: value,
-              })
-            }
+            onChange={dispatch}
             required={form.afternoonShiftLow.required}
           />
         </div>
@@ -185,7 +95,7 @@ Form.propTypes = {
 }
 
 function App() {
-  const [form, dispatch] = React.useReducer(formReducer, {
+  const [form, setForm] = React.useState({
     startDate: {
       value: '',
       required: true,
@@ -217,6 +127,17 @@ function App() {
     console.debug('Submit form')
   }
 
+  const onFieldChange = ({ target }) => {
+    const modifiedField = {
+      required: form[target.name].required,
+      value: target.value,
+    }
+    setForm({
+      ...form,
+      [target.name]: modifiedField,
+    })
+  }
+
   const areNsdConfigurationModalRequiedFieldFilled = (() => {
     const requiredFields = Object.keys(form).filter((key) => form[key].required)
     const areAllRequiredFieldsFilled = requiredFields.every(
@@ -229,10 +150,10 @@ function App() {
     <>
       <h1>Forms test</h1>
       <Form
+        dispatch={onFieldChange}
         form={form}
-        dispatch={dispatch}
-        onSubmit={onSubmit}
         isSubmitDisabled={areNsdConfigurationModalRequiedFieldFilled}
+        onSubmit={onSubmit}
       />
     </>
   )
